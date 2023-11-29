b8c1e6b3f675c78731b45701cc081b0891aadf93
황민영 <myhwang@hwangmin-yeongs-MacBook-Pro.local>
2023-11-29 20:38:37 +0900

    금주혜택 페이지(새로고침)

--- lib/screens/home/components/benefit_page.dart
+++ lib/screens/home/components/benefit_page.dart
@@ -1,12 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kurly/models/benefit_banner.dart';
+import 'package:flutter_kurly/theme.dart';
 
-class BenefitPage extends StatelessWidget {
+class BenefitPage extends StatefulWidget {
   const BenefitPage({super.key});
 
   @override
+  State<BenefitPage> createState() => _BenefitPageState();
+}
+
+class _BenefitPageState extends State<BenefitPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("BenefitPage"),
+    return RefreshIndicator(
+      onRefresh: () async {
+        await Future.delayed(const Duration(milliseconds: 1000));
+      },
+      child: ListView.builder(
+        itemCount: benefitBannerList.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: Container(
+              padding: const EdgeInsets.only(left: 22),
+              height: 140,
+              color: Color(0xeff4e4da * (index + 1)),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Spacer(),
+                      Text(
+                        "${benefitBannerList[index].title}",
+                        style: textTheme().bodyMedium,
+                      ),
+                      Text(
+                        "${benefitBannerList[index].benefit}",
+                        style: textTheme()
+                            .bodyMedium
+                            ?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      const Spacer(),
+                    ],
+                  ),
+                  const Spacer(),
+                  SizedBox(
+                    width: 115,
+                    height: 120,
+                    child: Image.asset(
+                      benefitBannerList[index].image,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
